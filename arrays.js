225603ad93f42af0d128c669280a15ca44046587
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-02 18:24:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,4 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   array.shift(); return array;}
 function removeElementFromBeginningOfArray (array) {array.slice(1); return array;}
 function destructivelyRemoveElementFromEndOfArray (array) {array.pop();return array;}
+function removeElementFromEndOfArray (array) {array.slice(0,array.length-1); return array;}
