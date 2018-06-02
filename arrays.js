bc96164e41893246d1c113879338681810e1bf18
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-02 18:28:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,4 @@ function removeElementFromBeginningOfArray (array) {
 function destructivelyRemoveElementFromEndOfArray (array) { array.pop();return array;}
 function removeElementFromEndOfArray (array) {
  return array.slice(0, array.length -1); return array;}
+ 
