5f30c8b3f24dc97ccda77b2b0c84e24511cdd936
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-02 18:23:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,4 @@ function accessElementInArray (array,index) {
 function destructivelyRemoveElementFromBeginningOfArray (array) {
   array.shift(); return array;}
 function removeElementFromBeginningOfArray (array) {array.slice(1); return array;}
+function destructivelyRemoveElementFromEndOfArray (array) {array.pop();return array;}
